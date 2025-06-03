63a542ea3640b4525dc510c042961f514f9461c5
TuRan <turanproc@outlook.com>
2025-06-03 09:22:29 +0800

    重构三数之和算法，优化代码结构，移除不必要的集合，改用向量存储结果，并添加重复元素检查以提高性能

--- 15.三数之和.cpp
+++ 15.三数之和.cpp
@@ -1,15 +1,13 @@
 // @before-stub-for-debug-begin
 #include "commoncppproblem15.h"
+#include <algorithm>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 // @before-stub-for-debug-end
 
-#include <algorithm>
-#include <set>
-#include <vector>
-using namespace std;
 /*
  * @lc app=leetcode.cn id=15 lang=cpp
  *
@@ -21,28 +19,31 @@ class Solution {
 public:
 	vector<vector<int>> threeSum(vector<int>& nums)
 	{
-		set<vector<int>> triples = {};
+		vector<vector<int>> triples = {};
+		sort(nums.begin(), nums.end());
 
-		// 逐条遍历 i, j, k
 		for (int i = 0; i < nums.size(); ++i) {
+			if (i > 0 && nums[i] == nums[i - 1])
+				continue;
+			int target = -nums[i];
+			int k = nums.size() - 1;
+
 			for (int j = i + 1; j < nums.size(); ++j) {
-				// if (nums[j] == nums[i])
-				// 	continue;
-				for (int k = j + 1; k < nums.size(); ++k) {
-					// if (nums[k] == nums[j])
-					// 	continue;
-					if (nums[i] + nums[j] + nums[k] == 0) {
-						vector<int> triple = { nums[i], nums[j], nums[k] };
-						// 将triple进行排序, 添加到triples集合中
-						sort(triple.begin(), triple.end());
-						triples.insert(triple);
-					}
-				}
+				if (j > i + 1 && nums[j] == nums[j -1])
+					continue;
+
+				while (j < k && nums[j] + nums[k] > target)
+					--k;
+
+				if (j == k)
+					break;
+
+				if (nums[j] + nums[k] == target)
+					triples.push_back({ nums[i], nums[j], nums[k] });
 			}
 		}
-
-		// 将triples转化为 vector<vector<int>> 类型
-		return vector<vector<int>>(triples.begin(), triples.end());
+		
+		return triples;
 	}
 };
 // @lc code=end
